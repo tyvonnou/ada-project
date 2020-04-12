47e251df80d519262a8b8e5afd13fe90d87c575e
Théo Yvonnou <tyvonnou@gmail.com>
2020-04-13 01:03:14 +0200

    [packages] - Mise en place de la structure

--- exemple.adb
+++ exemple.adb
@@ -2,7 +2,6 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Text_IO;               use Text_IO;
 with user_level_schedulers; use user_level_schedulers;
 with user_level_tasks;      use user_level_tasks;
-
 with my_subprograms; use my_subprograms;
 
 procedure example is
